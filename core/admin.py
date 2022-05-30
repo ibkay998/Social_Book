a578902b5baf504aa874a7b2d702d10f5d08b2b9
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-05-30 10:19:49 +0100

    social book app

---
+++ core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Profile,Post,LikePost
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(LikePost)
+
+# Register your models here.
