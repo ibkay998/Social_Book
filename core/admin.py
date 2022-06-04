7aa09250376b910f29c75ef11c880ee27147e610
ibkay998 <ibukun46@gmail.com>
2022-06-04 16:47:01 +0100

    Delete core directory

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Profile,Post,LikePost
-
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(LikePost)
-
-# Register your models here.
