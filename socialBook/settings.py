96ee9c17b202c42b04845d39509a04a28049ee21
ibkay998 <ibukun46@gmail.com>
2022-07-31 17:29:03 +0100

    Update settings.py

--- socialBook/settings.py
+++ socialBook/settings.py
@@ -32,9 +32,9 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["social-booknew.herokuapp.com","127.0.0.1"]
 
 
 # Application definition
